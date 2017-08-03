2ed777ee61235a6dd402c1c03bf351d4a707e46b
Scott Pfeiffer <scott.pfeiffer@globalfoundries.com>
2017-08-03 19:48:15 -0400

    m

--- ControlTower.js
+++ ControlTower.js
@@ -1,97 +1,190 @@
-class ControlTower {
-	constructor(url){
-		if(url){
-			this.url = url;
-		}else{
-			this.url = "ws://127.0.0.1:8000/";
+function ControlTower(config) {
+	this.counter = 0;
+	this.mailboxWatchers = {};
+	
+	this.config = config;
+	this.config.logging = (typeof this.config.logging === 'undefined') ? false : this.config.logging;
+	this.config.monitoringMode = (typeof this.config.monitoringMode === 'undefined') ? false : this.config.monitoringMode;
+	
+	//this.logging
+	//this.monitoringMode
+	//getter/setter
+
+	this.connect();
+};
+
+ControlTower.prototype.Package = function() {
+	var pkg = {};
+	pkg.id = this.counter++; 
+	return pkg;
+};
+
+ControlTower.prototype.connect = function(){
+	this.ws = new WebSocket(this.config.url);
+	this.ws.controller = this;
+
+	this.ws.onclose = function(event) {
+		console.log(event);
+		this.reconnect();
+	};
+
+	this.ws.onerror = function(event) {
+		console.log(event);
+		// this.reconnect();
+	};	
+
+	this.ws.onopen = function(event) {
+		// console.log(this, event);
+		var pkg = this.controller.Package();
+		pkg.type = "join";
+		pkg.logging = this.controller.config.logging;
+		pkg.monitoringMode = this.controller.config.monitoringMode;
+		this.send(JSON.stringify(pkg));
+	};
+
+	this.ws.reconnect = function(){
+		setTimeout(function(ct){
+			console.log("WS Reconnecting");
+			ct.connect(ct.url);
+			// instance.send("PROBE");
+		},5000,this.controller);
+	};
+
+	this.ws.onmessage = function(event) {
+		console.log("Global Listen",event.data);
+		var response = JSON.parse(event.data);
+
+		if("type" in response && response.type == "clientsMonitor"){
+			console.log("Monitoring",response)
 		}
 		
-		this.counter = 0;
-		this.mailboxWatchers = {};
+		if("type" in response && response.type == "log"){
+			if("logger" in this.controller.config){
+				this.controller.config.logger.call(this.controller,response);
+			}
+			console.log("CT Log",response.msg)
+		}
 		
-		this.connect();
-	};
-
-	Package() {
-		var pkg = {};
-		pkg.id = this.counter++; 
-		return pkg;
-	};
-
-	connect(){
-		this.ws = new WebSocket(this.url);
-		this.ws.controller = this;
-		
-		this.ws.onclose = function(event) {
-			console.log(event);
-			this.reconnect();
-		};
-		
-		this.ws.onerror = function(event) {
-			console.log(event);
-			// this.reconnect();
-		};	
-		
-		this.ws.onopen = function(event) {
-			// console.log(this, event);
+		if("type" in response && response.type == "scanRequest"){
 			var pkg = this.controller.Package();
-			pkg.type = "join";
+			pkg.type = "scanResult";
+			pkg.osid = response.osid;
+			pkg.response = JSON.decycle(this.controller);
 			this.send(JSON.stringify(pkg));
-		};
+		}
 		
-		this.ws.reconnect = function(){
-			setTimeout(function(ct){
-				console.log("WS Reconnecting");
-				ct.connect(ct.url);
-				// instance.send("PROBE");
-			},5000,this.controller);
-		};
+		if("type" in response && response.type == "commandRequest"){
+			var pkg = this.controller.Package();
+			pkg.type = "commandResult";
+			pkg.osid = response.osid;
+			pkg.response = JSON.decycle(eval(response.command));
+			this.send(JSON.stringify(pkg));
+		}
 		
-		this.ws.onmessage = function(event) {
-			console.log("Global Listen",event.data);
-			var response = JSON.parse(event.data);
-			
-			if("type" in response && response.type == "scan"){
-				this.send(JSON.stringify(JSON.decycle(this.controller)));
-			}
-			
-			if("id" in response && response.id in this.controller.mailboxWatchers){
-				this.controller.mailboxWatchers[response.id](response);
-			}
-		};
-		
+		if("type" in response && (response.type == "refreshPage" || response.type == "refreshPageAll")){
+			location.reload(false); //has option of forceGet full non-cache reload
+		}
+
+		if("id" in response && response.id in this.controller.mailboxWatchers){
+			this.controller.mailboxWatchers[response.id].call(this.controller,response);
+		}
 	};
 
-	request(pkg) {
-		return new Promise(resolve => {
-			this.mailboxWatchers[pkg.id] = function(response){
-				console.log('Client Request Response',pkg.id,response);
-				resolve(response);
-			}
-			this.ws.send(JSON.stringify(pkg));
-			/*
-			this.eventListeners[pkg.id] = this.ws.addEventListener('message', function (event) {
-				console.log('Client Request Response',pkg.id,event.data);
-				var response = JSON.parse(event.data);
-				if(response.id == pkg.id){
-					resolve(response);
-					
-				}else{
-					
-				}
-				console.log("ev",event.source,arguments);
-			});
-			*/
-		});	
-	}
+};
 
-	async clients(){
-		var pkg = this.Package();
-		pkg.type = "clients";
-		var response = await this.request(pkg);
-		console.log("async",response);
-		response.mark = true;
-		return response;
-	};
+ControlTower.prototype.request = function(pkg,callback) {
+	this.mailboxWatchers[pkg.id] = callback;	
+	this.ws.send(JSON.stringify(pkg));
+	return pkg.id;
+	//return new Promise(resolve => {
+		/*
+		this.mailboxWatchers[pkg.id] = function(response){
+			console.log('Client Request Response',pkg.id,response);
+			resolve(response);
+		}
+		this.ws.send(JSON.stringify(pkg));
+		*/
+		/*
+		 * this.eventListeners[pkg.id] = this.ws.addEventListener('message',
+		 * function (event) { console.log('Client Request
+		 * Response',pkg.id,event.data); var response =
+		 * JSON.parse(event.data); if(response.id == pkg.id){
+		 * resolve(response);
+		 * 
+		 * }else{
+		 *  } console.log("ev",event.source,arguments); });
+		 */
+	//});	
 }
+
+ControlTower.prototype.clients = function(){
+	var pkg = this.Package();
+	pkg.type = "clients";
+	var id = this.request(pkg,function(cb){
+		console.log(cb);
+	});
+	return id;
+};
+
+ControlTower.prototype.scan = function(sid){
+	var pkg = this.Package();
+	pkg.type = "scanRequest";
+	pkg.sid = sid;
+	var id = this.request(pkg,function(cb){
+		console.log(cb);
+	});
+	return id;
+};
+
+ControlTower.prototype.refreshPage = function(sid){
+	var pkg = this.Package();
+	pkg.type = "refreshPage";
+	pkg.sid = sid;
+	var id = this.request(pkg,function(cb){
+		console.log(cb);
+	});
+	return id;
+};
+
+ControlTower.prototype.refreshPageAll = function(){
+	var pkg = this.Package();
+	pkg.type = "refreshPageAll";
+	var id = this.request(pkg,function(cb){
+		console.log(cb);
+	});
+	return id;
+};
+
+ControlTower.prototype.clientsMonitor = function(mode){
+	var pkg = this.Package();
+	pkg.type = "clientsMonitor";
+	pkg.mode = mode;
+	var id = this.request(pkg,function(cb){
+		console.log(cb);
+	});
+	return id;
+};
+
+ControlTower.prototype.logging = function(mode){
+	var pkg = this.Package();
+	pkg.type = "logging";
+	pkg.mode = mode;
+	var id = this.request(pkg,function(cb){
+		console.log(cb);
+	});
+	return id;
+};
+
+ControlTower.prototype.command = function(sid,command){
+	var pkg = this.Package();
+	pkg.type = "commandRequest";
+	pkg.sid = sid;
+	pkg.command = command;
+	var id = this.request(pkg,function(cb){
+		console.log(cb);
+	});
+	return id;
+};
+
+
 
--- ControlTower.py
+++ ControlTower.py
@@ -1,51 +1,105 @@
 from SimpleWebSocketServer import SimpleWebSocketServer, WebSocket
 import json
+import socket
 
-clients = []
+
 
 class ControlTower(WebSocket):
-
-    def handleMessage(self):
-        print(self.address, self.data)
+    
+    def log(self,msg):
+        print(msg)
         
-        input = json.loads(self.data)
-
-        if input["type"] == "join":
-            input["response"] = "hey"
-            pkg = json.dumps(input)
-            self.sendMessage(pkg)
-        """ 
-        if input["type"] == "scan":
-            pkg = json.dumps(input)
-            self.sendMessage(pkg)
-        """
-            
-        if input["type"] == "clients":
-            pkg = json.dumps(input)
-            self.sendMessage(pkg)
-
-        for client in clients:
-            if client != self:
-                client.sendMessage(self.address[0] + u' - ' + self.data)
-
-
-    def handleConnected(self):
-        print(self.address, 'connected')
-        for client in clients:
-            client.sendMessage(self.address[0] + u' - connected')
-        clients.append(self)
-        
-        
-        pkg = {"type":"scan"}
-        print(type(pkg))
-        print(pkg)
+        for k,client in self.server.connections.items():
+            if client.logging:
+                self.sendMessage(json.dumps({"type":"log","msg":msg}))
+    
+    def send(self,pkg):
+        self.log(str(self.address)+" > "+str(pkg))
         self.sendMessage(json.dumps(pkg))
 
+    def handleMessage(self):
+        pkg = json.loads(self.data)
+        
+        
+        
+        #use event: for all of these - ditch the string go for logof object and print out strng made from it instead
+        #clientsMonitor -> loggerOnlyClients will restric what gets logged out if logger is true
+        
+        self.log(str(self.address)+" < "+str(pkg))
+
+        if pkg["type"] == "join":
+            pkg["response"] = "hey"
+            self.logging = pkg["logging"]
+            self.monitoringMode = pkg["monitoringMode"]
+            #print(socket.gethostbyaddr(self.address[0]),socket.getfqdn(self.address[0]))
+
+            pkg["client"] = self.info
+            #self.sendMessage(json.dumps(pkg))
+            self.send(pkg)
+        
+        if pkg["type"] == "clientsMonitor":  
+            self.monitoringMode = pkg["mode"]
+            
+        if pkg["type"] == "logging":  
+            self.logging = pkg["mode"]
+            
+        if pkg["type"] == "refreshPage":
+            pkg["osid"] = self.sid    
+            self.server.connections[pkg["sid"]].send(pkg)
+            
+        if pkg["type"] == "scanRequest":
+            pkg["osid"] = self.sid    
+            self.server.connections[pkg["sid"]].send(pkg)
+            
+        if pkg["type"] == "scanResult":
+            self.server.connections[pkg["osid"]].send(pkg)
+            
+        if pkg["type"] == "commandRequest":
+            pkg["osid"] = self.sid    
+            self.server.connections[pkg["sid"]].send(pkg)
+            
+        if pkg["type"] == "commandResult":
+            self.server.connections[pkg["osid"]].send(pkg)
+        
+            
+        if pkg["type"] == "clients":
+            pkg["clients"] = []
+            for k,client in self.server.connections.items():
+                pkg["clients"].append(client.info)
+            self.send(pkg)
+            
+        if pkg["type"] == "refreshPageAll":
+            for k,client in self.server.connections.items():
+                if client.logging == False and client.monitoringMode == False:
+                    client.send(pkg)
+
+
+    def handleConnected(self): 
+        self.sid = self.client.fileno()
+        self.address = self.address + (socket.getfqdn(self.address[0]),) + (self.sid,)
+        self.monitoringMode = False
+        self.logging = False
+        
+        self.info = {"ip":self.address[0],"port":self.address[1],"host":self.address[2],"sid":self.sid}
+        self.log(str(self.address)+" connected")
+        
+        for k,client in self.server.connections.items():
+            if client.monitoringMode:
+                client.send({"type":"clientsMonitor","client":client.info,"event":"connected"})
+
+        
+
     def handleClose(self):
-        clients.remove(self)
-        print(self.address, 'closed')
-        for client in clients:
-            client.sendMessage(self.address[0] + u' - disconnected')
+        self.log(str(self.address)+" closed")
+        
+        for k,client in self.server.connections.items():
+            if client.monitoringMode:
+                client.send({"type":"clientsMonitor","client":client.info,"event":"closed"})
+
 
 server = SimpleWebSocketServer('', 8000, ControlTower)
 server.serveforever()
+
+def refreshPages():
+    for k,client in server.items():
+        print(k,client)
